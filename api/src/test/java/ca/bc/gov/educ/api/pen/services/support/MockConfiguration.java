25c64fbd7978b2866a61b364f4ce664f1fadde69
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-10-15 16:31:23 -0700

    test fixes...

--- api/src/test/java/ca/bc/gov/educ/api/pen/services/support/MockConfiguration.java
+++ api/src/test/java/ca/bc/gov/educ/api/pen/services/support/MockConfiguration.java
@@ -1,9 +1,12 @@
 package ca.bc.gov.educ.api.pen.services.support;
 
+import ca.bc.gov.educ.api.pen.services.config.RedissonSpringDataConfig;
 import ca.bc.gov.educ.api.pen.services.messaging.MessagePublisher;
 import ca.bc.gov.educ.api.pen.services.messaging.MessageSubscriber;
 import ca.bc.gov.educ.api.pen.services.rest.RestUtils;
 import org.mockito.Mockito;
+import org.redisson.api.RedissonClient;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -48,4 +51,38 @@ public class MockConfiguration {
     return Mockito.mock(MessageSubscriber.class);
   }
 
+
+  /**
+   * Redisson spring data config redisson spring data config.
+   *
+   * @return the redisson spring data config
+   */
+  @Bean
+  @Primary
+  public RedissonSpringDataConfig redissonSpringDataConfig() {
+    return Mockito.mock(RedissonSpringDataConfig.class);
+  }
+
+
+  /**
+   * Redisson connection factory redisson connection factory.
+   *
+   * @return the redisson connection factory
+   */
+  @Bean
+  @Primary
+  public RedissonConnectionFactory redissonConnectionFactory() {
+    return Mockito.mock(RedissonConnectionFactory.class);
+  }
+
+  /**
+   * Redisson client redisson client.
+   *
+   * @return the redisson client
+   */
+  @Bean
+  @Primary
+  public RedissonClient redissonClient() {
+    return Mockito.mock(RedissonClient.class);
+  }
 }
