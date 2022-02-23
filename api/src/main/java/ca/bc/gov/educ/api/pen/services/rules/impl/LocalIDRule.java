aede24a1fe2006d3656d4b4da267cb40082e39dc
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-02-23 14:57:11 -0800

    Added validation rule for invalid characters; removed postal code warning.

--- api/src/main/java/ca/bc/gov/educ/api/pen/services/rules/impl/LocalIDRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/pen/services/rules/impl/LocalIDRule.java
@@ -5,24 +5,21 @@ import ca.bc.gov.educ.api.pen.services.struct.v1.PenRequestStudentValidationIssu
 import ca.bc.gov.educ.api.pen.services.struct.v1.PenRequestStudentValidationPayload;
 import com.google.common.base.Stopwatch;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
-import static ca.bc.gov.educ.api.pen.services.constants.PenRequestStudentValidationFieldCode.POSTAL_CODE;
+import static ca.bc.gov.educ.api.pen.services.constants.PenRequestStudentValidationFieldCode.LOCAL_ID;
 import static ca.bc.gov.educ.api.pen.services.constants.PenRequestStudentValidationIssueSeverityCode.ERROR;
-import static ca.bc.gov.educ.api.pen.services.constants.PenRequestStudentValidationIssueSeverityCode.WARNING;
 import static ca.bc.gov.educ.api.pen.services.constants.PenRequestStudentValidationIssueTypeCode.INV_CHARS;
-import static ca.bc.gov.educ.api.pen.services.constants.PenRequestStudentValidationIssueTypeCode.PC_ERR;
 
 /**
- * The type Postal code rule.
+ * The type Local ID rule.
  */
 @Slf4j
-public class PostalCodeRule extends BaseRule {
+public class LocalIDRule extends BaseRule {
+
 
   /**
    * Validates the student record for the given rule.
@@ -34,9 +31,9 @@ public class PostalCodeRule extends BaseRule {
   public List<PenRequestStudentValidationIssue> validate(final PenRequestStudentValidationPayload validationPayload) {
     final var stopwatch = Stopwatch.createStarted();
     final List<PenRequestStudentValidationIssue> results = new LinkedList<>();
-    final String postalCode = validationPayload.getPostalCode();
-    if (this.fieldContainsNonAsciiCharacter(postalCode)) {
-      results.add(this.createValidationEntity(ERROR, INV_CHARS, POSTAL_CODE));
+    final String localID = validationPayload.getLocalID();
+    if (this.fieldContainsNonAsciiCharacter(localID)) {
+      results.add(this.createValidationEntity(ERROR, INV_CHARS, LOCAL_ID));
     }
     log.debug("transaction ID :: {} , returning results size :: {}", validationPayload.getTransactionID(), results.size());
     stopwatch.stop();
--- api/src/test/java/ca/bc/gov/educ/api/pen/services/rules/impl/LocalIDRuleTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/pen/services/rules/impl/LocalIDRuleTest.java
@@ -16,9 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  * The type Postal code rule test.
  */
 @RunWith(JUnitParamsRunner.class)
-public class PostalCodeRuleTest {
+public class LocalIDRuleTest {
 
-  private PostalCodeRule rule;
+  private LocalIDRule rule;
 
 
   /**
@@ -27,13 +27,13 @@ public class PostalCodeRuleTest {
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
-    rule = new PostalCodeRule();
+    rule = new LocalIDRule();
   }
 
   /**
    * Validate.
    *
-   * @param postalCode     the postal code
+   * @param localID     the postal code
    * @param expectedErrors the expected errors
    */
   @Test
@@ -42,22 +42,20 @@ public class PostalCodeRuleTest {
       ", 0",
       "V8W2E1, 0",
       "V8R4N4, 0",
-      "123546, 1",
-      "123, 1",
-      "1234, 1",
-      "12354, 1",
-      "123456, 1",
-      "1234567, 1",
-      "12345678,1",
-      "1234567890454, 1",
-      "INVALIDPOSTAL, 1",
-      "V8R4Æ4, 1"
+      "123546, 0",
+      "123, 0",
+      "1234, 0",
+      "12354, 0",
+      "123456, 0",
+      "1234567, 0",
+      "12345678,0",
+      "A123Æ4, 1"
   })
-  public void validate(String postalCode, int expectedErrors) {
-    if ("null".equals(postalCode)) {
-      postalCode = null;
+  public void validate(String localID, int expectedErrors) {
+    if ("null".equals(localID)) {
+      localID = null;
     }
-    PenRequestStudentValidationPayload payload = PenRequestStudentValidationPayload.builder().isInteractive(false).transactionID(UUID.randomUUID().toString()).postalCode(postalCode).build();
+    PenRequestStudentValidationPayload payload = PenRequestStudentValidationPayload.builder().isInteractive(false).transactionID(UUID.randomUUID().toString()).localID(localID).build();
     var result = rule.validate(payload);
     assertThat(result).size().isEqualTo(expectedErrors);
   }
